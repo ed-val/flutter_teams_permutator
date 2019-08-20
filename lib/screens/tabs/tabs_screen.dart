f8c7563bc0ebe45607bb63cd44b6d0f9005bd0e3
ed-val2 <e.valbuena.d@gmail.com>
2019-08-20 15:01:44 -0500

    got appbar height, removed unused libraries

--- lib/screens/tabs/tabs_screen.dart
+++ lib/screens/tabs/tabs_screen.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:teams_permutator/screens/home/home_screen.dart';
 import 'package:teams_permutator/screens/permutator/permutator_screen.dart';
 import 'package:teams_permutator/screens/singleTeam/single_team_screen.dart';
-import 'package:teams_permutator/utils/page_transitions.dart';
 
 class TabsScreen extends StatefulWidget {
   static const routeName = '/tabs';
@@ -17,44 +15,49 @@ class _TabsScreenState extends State<TabsScreen>
       CupertinoTabController(initialIndex: 0);
   PageController _pageController = PageController(initialPage: 0);
   List<dynamic> tabList = [PermutatorScreen(), SingleTeamScreen()];
+  int tabBarHeight = 0;
   double currentPageView = 0.0;
-  bool isPageCanChanged = true;
 
   @override
   void initState() {
-    _pageController.addListener(() {
-      setState(() {
-        currentPageView = _pageController.page;
-      });
-    });
     super.initState();
   }
 
-  void manualTranstion(int index) {
-    if (_pageController.hasClients) {
-      _pageController.animateToPage(
-        index,
-        duration: const Duration(milliseconds: 400),
-        curve: Curves.easeInOut,
-      );
-    }
-  }
-
-  // onPageChange(int index, {PageController p, TabController t}) async {
-  //   if (p != null) {
-  //     //判断是哪一个切换
-  //     isPageCanChanged = false;
-  //     await mPageController.animateToPage(index,
-  //         duration: Duration(milliseconds: 500),
-  //         curve: Curves.ease); //等待pageview切换完毕,再释放pageivew监听
-  //     isPageCanChanged = true;
-  //   } else {
-  //     mTabController.animateTo(index); //切换Tabbar
-  //   }
-  // }
-
   @override
   Widget build(BuildContext context) {
+    CupertinoTabBar _cupertinoTabBar = CupertinoTabBar(
+      onTap: (int index) {
+        switch (index) {
+          case 0:
+            manualTranstion(index);
+            break;
+          case 1:
+            manualTranstion(index);
+            break;
+        }
+        return null;
+      },
+      border: const Border(
+        top: BorderSide(
+            color: Colors.transparent, width: 0.0, style: BorderStyle.solid),
+      ),
+      activeColor: Colors.blue,
+      backgroundColor: Colors.transparent, //get blurr efect
+      inactiveColor: Colors.grey[300],
+      items: <BottomNavigationBarItem>[
+        BottomNavigationBarItem(
+          backgroundColor: Theme.of(context).primaryColor,
+          icon: Icon(CupertinoIcons.shuffle),
+          title: Text('Permutator'),
+        ),
+        BottomNavigationBarItem(
+          backgroundColor: Theme.of(context).primaryColor,
+          icon: Icon(CupertinoIcons.group_solid),
+          title: Text('Single Team'),
+        ),
+      ],
+    );
+
     return Column(
       children: <Widget>[
         Expanded(
@@ -84,40 +87,7 @@ class _TabsScreenState extends State<TabsScreen>
           child: CupertinoTabScaffold(
             resizeToAvoidBottomInset: true,
             controller: _cupertinoTabController,
-            tabBar: CupertinoTabBar(
-              onTap: (int index) {
-                switch (index) {
-                  case 0:
-                    manualTranstion(index);
-                    break;
-                  case 1:
-                    manualTranstion(index);
-                    break;
-                }
-                return null;
-              },
-              border: const Border(
-                top: BorderSide(
-                    color: Colors.transparent,
-                    width: 0.0,
-                    style: BorderStyle.solid),
-              ),
-              activeColor: Colors.blue,
-              backgroundColor: Colors.transparent, //get blurr efect
-              inactiveColor: Colors.grey[300],
-              items: <BottomNavigationBarItem>[
-                BottomNavigationBarItem(
-                  backgroundColor: Theme.of(context).primaryColor,
-                  icon: Icon(CupertinoIcons.shuffle),
-                  title: Text('Permutator'),
-                ),
-                BottomNavigationBarItem(
-                  backgroundColor: Theme.of(context).primaryColor,
-                  icon: Icon(CupertinoIcons.group_solid),
-                  title: Text('Single Team'),
-                ),
-              ],
-            ),
+            tabBar: _cupertinoTabBar,
             tabBuilder: (BuildContext context, int index) {
               switch (index) {
                 case 0:
@@ -137,6 +107,16 @@ class _TabsScreenState extends State<TabsScreen>
         ),
       ],
     );
+  }
+
+  void manualTranstion(int index) {
+    if (_pageController.hasClients) {
+      _pageController.animateToPage(
+        index,
+        duration: const Duration(milliseconds: 400),
+        curve: Curves.easeInOut,
+      );
+    }
   }
 
   Widget buildDummyTab(BuildContext context) {
