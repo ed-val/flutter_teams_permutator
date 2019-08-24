998e2ca45375f70c79a906d20e6bc4f5773cbd75
ed-val2 <e.valbuena.d@gmail.com>
2019-08-23 23:12:43 -0500

    change navbar on homescreen

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -39,9 +39,7 @@ class _HomeScreenState extends State<HomeScreen> {
           CustomScrollView(
             physics: AlwaysScrollableScrollPhysics(),
             slivers: <Widget>[
-              SliverToBoxAdapter(
-                child: buildCupertinoNavigationBar(),
-              ),
+              _buildCupertinoNavigationBar(),
               SliverGrid.count(
                 // this aspect ratio WILL DETERMINE CONTAINER'S SIZE (height) !!!!
                 childAspectRatio: 40 / 25,
@@ -72,8 +70,32 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 
-  Widget buildCupertinoNavigationBar() {
-    return CupertinoNavigationBar(
+  Widget _buildCupertinoNavigationBar() {
+    return CupertinoSliverNavigationBar(
+      largeTitle: Text('Willkommen'),
+      trailing: CupertinoButton(
+        child: Container(
+          // compensate for unavoidable parent's padding
+          transform: Matrix4.translationValues(0.0, -10, 0.0),
+          child: Text(
+            'Teams',
+            // fixed trimmed text caused for parent's padding
+            overflow: TextOverflow.visible,
+          ),
+        ),
+        onPressed: () {
+          Navigator.pushReplacement(
+            context,
+            CupertinoPageRoute(
+              builder: (ctx) => TabsScreen(),
+            ),
+            // TabsScreen.routeName
+          );
+        },
+      ),
+    );
+
+    CupertinoNavigationBar(
       backgroundColor: Colors.grey[50],
       automaticallyImplyLeading: true,
       middle: Text('Willkommen'),
